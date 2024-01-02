754c09e544e6d0ffe4e01e0fcf176caf94d2289f
Saeb Swaity <saebswaity1010@gmail.com>
2024-01-02 22:11:52 +0200

    add main.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# calculator" 
+# Calculator 
+
+This Python project showcases a simple calculator using Object-Oriented Programming (OOP) and Functional Programming paradigms. The `Process` class handles state and operations in the OOP style, while lambda functions and composition illustrate the functional approach. Explore the examples for usage insights.
---
+++ main.py
@@ -0,0 +1,70 @@
+from functools import reduce
+
+class Process:
+    def __init__(self, initial_state=0):
+        self.state = initial_state
+
+    @staticmethod
+    def compose_functions(func_list):
+        return lambda x: reduce(lambda v, f: f(v), func_list, x)
+
+    @staticmethod
+    def add(value):
+        return lambda x: x + value
+
+    @staticmethod
+    def subtract(value):
+        return lambda x: x - value
+
+    @staticmethod
+    def multiply(value):
+        return lambda x: x * value
+
+    @staticmethod
+    def divide(value):
+        if value != 0:
+            return lambda x: x / value
+        else:
+            print("Error: Cannot divide by zero")
+
+    def set_state(self, update_lambda):
+        if update_lambda is not None:
+            self.state = update_lambda(self.state)
+
+    def get_state(self):
+        return self.state
+
+# Example usage:
+process = Process(initial_state=10)
+
+print("Initial State:", process.get_state())  # Output: Initial State: 10
+
+add_5 = Process.add(5)
+process.set_state(add_5)
+print("After Adding 5:", process.get_state())  # Output: After Adding 5: 15
+
+subtract_3 = Process.subtract(3)
+process.set_state(subtract_3)
+print("After Subtracting 3:", process.get_state())  # Output: After Subtracting 3: 12
+
+multiply_2 = Process.multiply(2)
+process.set_state(multiply_2)
+print("After Multiplying by 2:", process.get_state())  # Output: After Multiplying by 2: 24
+
+divide_4 = Process.divide(4)
+process.set_state(divide_4)
+print("After Dividing by 4:", process.get_state())  # Output: After Dividing by 4: 6
+
+# Example usage:
+process2 = Process(initial_state=10)
+
+print("Initial State:", process2.get_state())  # Output: Initial State: 10
+
+add_5 = Process.add(5)
+subtract_3 = Process.subtract(3)
+multiply_2 = Process.multiply(2)
+divide_4 = Process.divide(4)
+
+compose_operations = Process.compose_functions([add_5, subtract_3, multiply_2, divide_4])
+process2.set_state(compose_operations)
+print("After Composing Operations:", process2.get_state())  # Output: After Composing Operations: 6
